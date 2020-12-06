9053f663e3862397dbcea6be750a421672d6cf5f
elina-mns <ellie.mansurova@gmail.com>
2020-12-06 14:22:34 -0500

    Initial Commit

---
+++ BillSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BillSplit
+//
+//  Created by Elina Mansurova on 2020-12-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
